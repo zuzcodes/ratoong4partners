9f0ae5a98c6bb23f1f294557365b0334730ac277
Zuz <zuza0355@stud.kea.dk>
2021-12-16 14:58:26 +0100

    back

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,8 +1,6 @@
-//import React, { useState } from "react";
-//import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Button } from "../../globalStyles";
-import validate from '../validateForm';
-import useForm from '../useForm';
 import {
   SignUpSection,
   SignUpFormContainer,
@@ -11,49 +9,25 @@ import {
   SignUpFormName,
   SignUpFormEmail,
   SignUpFormPassword,
-  Error
 } from "./SignUpForm.elements";
 
-const SignUpForm = ( { submitForm } ) => {
- const { handleChange, handleSubmit, values, errors } = useForm(
-    submitForm,
-    validate
-  );   
+function SignUpForm() {
+  const [email, setEmail] = useState("");
+  const emailChange = (e) => setEmail(e.target.value);
+
   return (
     <SignUpSection>
       <SignUpFormContainer>
         <SignUpFormHeading>SIGN UP</SignUpFormHeading>
         <SignUpFormSubheading>Join the snow partnership now!</SignUpFormSubheading>
-        <form onSubmit={handleSubmit} className='form' noValidate>
-        <Error>{errors.name && <p>{errors.name}</p>}</Error>
-        <SignUpFormName 
-            type='text'
-            name='name'
-            placeholder='Name'
-            value={values.username}
-            onChange={handleChange} />
-        <Error>{errors.email && <p>{errors.email}</p>}</Error>
-        <SignUpFormEmail
-            type='email'
-            name='email'
-            placeholder='Email'
-            value={values.email}
-            onChange={handleChange}
-         />
-        <Error>{errors.password && <p>{errors.password}</p>}</Error>
-        <SignUpFormPassword 
-            type='password'
-            name='password'
-            placeholder='Password'
-            value={values.password}
-            onChange={handleChange}
-            />
-       
+        <SignUpFormName name="name" type="text" placeholder="Name" required />
+        <SignUpFormEmail name="email" type="email" placeholder="Email" value={email} onChange={emailChange} required />
+        <SignUpFormPassword name="password" type="password" placeholder="Password" required />
+        <Link to="/sign-up-confirmation">
           <Button primary display wide type="submit">
             SIGN UP
           </Button>
-       
-        </form>
+        </Link>
       </SignUpFormContainer>
     </SignUpSection>
   );
