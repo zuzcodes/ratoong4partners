84366a610de504b8810b7441c5c58a4d189e8789
Zuz <zuza0355@stud.kea.dk>
2021-12-17 06:27:42 +0100

    format

--- src/components/useContact.js
+++ src/components/useContact.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 const useContact = (callback, validate) => {
-  console.log("!!!!", callback);
   const [value, setValue] = useState({
     email: "",
   });
